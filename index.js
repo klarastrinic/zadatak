0a490bbd18c1aef53dcb2bb066238f2d8d474424
klarastrinic <45819882+klarastrinic@users.noreply.github.com>
2020-04-29 11:18:08 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script>
+        var scrambled = ["akmsuzija","avakr","orme","ašklo","mijaz","žnea","saec", "Ašklo ej odasdan", "Lkara ej xetra"];
+        var answers = ["maksuzija","krava","more","škola","zmija","žena","case", "Škola je dosadna", "Klara je extra"];
+
+        var random = Math.floor((Math.random() * 9));
+        $(".scrambled").text(scrambled[random]);
+        $("#joke-form").on("submit", function(event){
+            event.preventDefault();
+            var $answer = $(this).find('[type=text]');  
+            var answer = $answer.val();           
+            if (answer === answers[random]) {
+                $('.correct').css("display", "block").fadeOut(2500);
+                $answer.val("");
+                random = Math.floor((Math.random() * 5));
+                $(".scrambled").text(scrambled[random]);
+            } else {
+                $(".incorrect").css("display", "block").fadeOut(2500);
+            }
+        });
+        
+    </script>
+    </body>
+</html>
